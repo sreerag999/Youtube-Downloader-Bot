22d73de7e684d14b1797c92f6f086cffdd015748
sreerag999 <80700487+sreerag999@users.noreply.github.com>
2021-06-07 21:18:12 +0530

    sk.py

--- plugins/start.py
+++ plugins/start.py
@@ -5,9 +5,9 @@ from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, Inl
 async def start(client, message):
     # return
     joinButton = InlineKeyboardMarkup([
-        [InlineKeyboardButton("Channel", url="https://t.me/aryan_bots")],
-        [InlineKeyboardButton(
-            "Report Bugs 😊", url="https://t.me/aryanvikash")]
+        [InlineKeyboardButton("Contact Owner", url="https://t.me/Sreekuttan_TG")],
+        [InlineKeyboardButton
+            "Movie Group 😊", url="https://t.me/Sk_Movies_Chat")]
     ])
     welcomed = f"Hey <b>{message.from_user.first_name}</b>\n/help for More info"
     await message.reply_text(welcomed, reply_markup=joinButton)
